4c13ccd443219d2b88382874f661a6286f77f296
beemaningit <krisfurryface10@proton.me>
2025-05-04 05:10:25 -0700

    Update musicCycler.sh

--- musicCycler.sh
+++ musicCycler.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-filename='./musicCatalog/*'
+filename='./musicCatalog'
 clockLength='100'
 videoPlayer='mpv'
-while getopts "hcvkpf:t:" flag; do
+while getopts "hcvkpnf:t:" flag; do
   case $flag in
     h)
         echo "This program cycles through songs and notifies the user when it does"
+        echo "Unique Dependencys:"
+        echo "  mpv"
+        echo "  ffprobe"
+        echo "  notify-send"
         echo " "
         echo "flags:"
         echo "  {-h} Help"
@@ -13,7 +17,8 @@ while getopts "hcvkpf:t:" flag; do
         echo "  {-v} enables video playback"
         echo "  {-k} stops the clear command from being used"
         echo "  {-p} uses device video player, most players need -v flag"
-        echo "  {-f [directory]} specify where to play from defaults to $filename"
+        echo "  {-n} disables notifications"
+        echo "  {-f [folder]} specify where to play from defaults to $filename"
         echo "  {-t [int]} takes a percent of time 1000 is 100% and 10 is 1%"
         exit 1
         ;;
@@ -30,8 +35,11 @@ while getopts "hcvkpf:t:" flag; do
         videoPlayer='xdg-open'
         # set to use default player instead of mpv
         ;;
+    n)
+        # disables notifications
+        ;;
     f)
-        filename="$OPTARG/*"
+        filename="$OPTARG"
         # Process the specified folder
         ;;
     t)
@@ -44,9 +52,10 @@ while getopts "hcvkpf:t:" flag; do
        ;;
   esac
 done
+#shows that the script did something
 echo "checking every file in $filename"
 while true; do
-    for file in $filename; do
+    for file in "$filename"/*; do
         if [ -f "$file" ]; then
             if [[ $* != *-k* ]]; then
                 clear
@@ -55,19 +64,27 @@ while true; do
             echo "$(basename "${file::-4}")"
             #uses ffprobe to probe the time in decimal milliseconds
             time="$(ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")"
+            #converts the time to a int and multiplies it by configurable amount
             clockTime=$(($(printf "%.0f" "$time") * $clockLength))
-            echo "Notification length: $clockTime or $(($clockLength / 10))% total time"
-            echo "$(basename "${file::-4}")" | while read OUTPUT; do notify-send -t "$clockTime" --hint=int:transient:1 'Now Playing' "$OUTPUT"; done
+            if [[ $* != *-n* ]]; then
+                #notifies the user when a new song starts
+                echo "Notification length: $clockTime or $(($clockLength / 10))% total time"
+                echo "$(basename "${file::-4}")" | while read OUTPUT; do notify-send -t "$clockTime" --hint=int:transient:1 'Now Playing' "$OUTPUT"; done
+            fi
             if [[ $* == *-v* ]]; then
                 $videoPlayer "$file"
             else
+                #removes video so that the song gets played in the background
                 $videoPlayer --no-video "$file"
             fi
         else
+            #lets the user know that nested folders don't work
             echo "skipping folder: $file"
+            sleep 0.1s
         fi
     done
+    #stops the program when 
     if [[ $* == *-c* ]]; then
-        break
+        exit 1
     fi
 done
