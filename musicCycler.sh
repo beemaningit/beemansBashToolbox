90d57ea5232f6ac752536589af468871625b8f78
beemaningit <krisfurryface10@proton.me>
2025-05-04 00:33:28 -0700

    Added Music Cycler
    This is the first release of the script music cycler, the Linux script that allows people to cycle songs in the terminal, and this script is able to notify the user when the song changes with a configurable amount of time.

---
+++ musicCycler.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+filename='./musicCatalog/*'
+clockLength='100'
+while getopts "hcvkf:t:" flag; do
+  case $flag in
+    h)
+        echo "flags:"
+        echo "{-h} Help"
+        echo "{-s} stops at end of the list"
+        echo "{-v} enables video playback, causes problems"
+        echo "{-k} stops the clear command from being used"
+        echo "{-f [directory]} specify where to play from defaults to ./musicCatalog/*"
+        echo "{-t [int]} takes a percent of time 1000 is 100% and 10 is 1%"
+        echo "This program cycles through songs and notifies the user when it does"
+        exit 1
+        ;;
+    c)
+        # stop at end of list
+        ;;
+    v)
+        # enables video playback
+        ;;
+    k)
+        # don't clear lines after each song
+        ;;
+    f)
+        filename="$OPTARG/*"
+        # Process the specified folder
+        ;;
+    t)
+        clockLength="$OPTARG"
+        # % of time to show notification
+        ;;
+    \?) # Handle invalid options
+        echo "invalid flag -h for help"
+        exit 1
+       ;;
+  esac
+done
+echo "checking every file in $filename"
+while true; do
+    for file in $filename; do
+        if [ -f "$file" ]; then
+            if [[ $* != *-k* ]]; then
+                clear
+            fi
+            echo 'Now Playing:'
+            echo "$(basename "${file::-4}")"
+            #uses ffprobe to probe the time in decimal milliseconds
+            time="$(ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")"
+            clockTime=$(($(printf "%.0f" "$time") * $clockLength))
+            echo "Notification length: $clockTime"
+            echo "$(basename "${file::-4}")" | while read OUTPUT; do notify-send -t "$clockTime" --hint=int:transient:1 'Now Playing' "$OUTPUT"; done
+            if [[ $* == *-v* ]]; then
+                mpv "$file"
+            else
+                mpv --no-video "$file"
+            fi
+        else
+            echo "skipping folder: $file"
+        fi
+    done
+    if [[ $* == *-c* ]]; then
+        break
+    fi
+done
